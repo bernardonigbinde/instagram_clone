2e21db7b1bce3d6a38d10818b8e02220b19a6c9b
bernardonigbinde <bernard.onigbinde@gmail.com>
2024-09-20 16:01:46 +0200

    Initial commit. Icons imported.

---
+++ lib/ui/theme/app_typography.dart
@@ -0,0 +1,21 @@
+// usernameStyle
+// sectionHeading - semibold/16/height: 21/spacing: -0.33px/#262626|#F9F9F9
+// sectionSubHeading - regular/15/height: 20/spacing: -0.25px/#262626|#F9F9F9
+// chip
+// storyUsernameStyle
+// bodyText - regular/13/spacing: -0.07px/#262626|#F9F9F9
+// textButtonStyle
+
+// subTitle - regular/12/height: 17/spacing: 0/#262626|#F9F9F9
+// subTitleLink - regular/12/height: 17/spacing: 0/#05386B|#F9F9F9
+
+// outlinedButtonStyle - bg: #FFFFFF|#000000/border: 1px/corner: 6/ stroke: #3C3C43 @ 18%|#FFFFFF @ 15%
+// outlinedButtonTextStyle - semibold/13/height: 18/spacing: -0.1px/#262626|#F9F9F9
+
+
+
+// searchTextFieldBackground: #767680 @ 12%|#262626
+// searchTextFieldTextStyle: #3C3C43 @ 60%|#8E8E93/regular/16/height: 22/spacing: -0.3px
+
+// headerBackground: #FAFAFA|#121212
+// bodyBackground: #FFFFFF|#000000
